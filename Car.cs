1f0801afadace7c3e43c5d290224d9b719c099ed
EminliAysun05 <aeminli232@gmail.com>
2024-06-24 16:25:21 +0400

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,73 @@
+﻿
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace TaskCar
+{
+    public class Car
+    {
+        // bu masin indiye qeder ne qeder gedib
+        private double Millage { get; set; }
+        // ne qeder benzin var onu hesablayir
+        private double Fuel { get; set; }
+        // 100den ne qeder benzin yandirib
+        private double  FuelComsuption { get; set; }
+
+        private double TankCapacity { get; set; }
+
+        public Car(double fuel, double fuelConsuption, double tankCapacity)
+        {
+            // millage onsuz sifirdi deye ctor'a vermirik
+            Fuel = fuel;
+            FuelComsuption = fuelConsuption;
+            TankCapacity = tankCapacity;
+        }
+        public Car() //default olacaq gelen deyerlerdir, ctor overloaddir
+        {
+            Fuel = 20;
+            TankCapacity = 40;
+            FuelComsuption = 10;
+        }
+
+        public bool Drive(double kilometer)
+        {
+            double needFuel = kilometer * FuelComsuption / 100;
+            if(Fuel>+needFuel) 
+            { 
+            Fuel-=needFuel;
+            Millage += kilometer;
+                Console.WriteLine($"{kilometer} yol gedildi, benzin {Fuel}");
+                return true;
+            }
+            Console.WriteLine($"Kifayet qeder benzin yoxdur.{needFuel} qeder benzin lazimdir, sizde {Fuel} qeder var");
+            return false;
+       }
+        public bool Refuel(double Amount)//ne qeder benzin doldura biler
+        {
+            if (Fuel + Amount <= TankCapacity)
+            {
+                Fuel += Amount;
+                Console.WriteLine($"{Amount} qeder benzin dolduruldu, bak {Fuel}");
+                return true;
+                
+            }
+            else
+            {
+                Console.WriteLine($"{TankCapacity-Fuel} qeder bos yeriniz var, hal hazirda daxil etdiyiniz miqdari doldura bilmersiniz");
+                return false;
+            }
+            
+        }
+        public void ShowFuel()
+        {
+            Console.WriteLine($"bak {Fuel}");
+        }
+        public void ShowMillage()
+        {
+            Console.WriteLine($" Km {Millage}");
+        }
+
+
+    }
+   
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Net.Http.Headers;
+
+namespace TaskCar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bankda ne qeder benzin var?");
+            double fuel = double.Parse(Console.ReadLine());
+            restart:
+            Console.WriteLine("Bakin hecmi ne qederdir?");
+            double capacity = double.Parse(Console.ReadLine());
+            
+            if(fuel> capacity)
+            {
+                Console.WriteLine("Hecm benzinden kicik olmamalidir");
+                goto restart;
+            }
+
+            Console.WriteLine("Masin 100 kilometere ne qeder benzin yandirir");
+             
+            double comsuption = double.Parse(Console.ReadLine());
+
+           Car car = new Car(fuel, comsuption, capacity);
+             restartApp:
+            Console.WriteLine("1. Sur\n 2. Zaprafkaya gir\n 3.Getdiyin yolu goster\n 0. exit");
+            string result = Console.ReadLine();
+           switch(result)
+            {
+                case "1":
+                    Console.WriteLine("Nece km yol getmek isteyirsen?");
+                    double kilometer = double.Parse(Console.ReadLine());
+                    car.Drive(kilometer);
+                    break;
+                case "2":
+                    Console.WriteLine("Ne qeder yanacaq doldurmaq isteyirsen?");
+                    double amount = double.Parse(Console.ReadLine());
+                    car.Refuel(amount);
+                    break;
+                case "3":
+                    car.ShowFuel();
+                    break;
+                case "4":
+                    car.ShowMillage();
+                    break;
+                case "0":
+                    Console.WriteLine("Program dayandi...");
+                    return;
+                    break;
+                default:
+                    Console.WriteLine("Duzgun deyer daxil edin");
+                    break;
+                
+                    
+            }
+             
+            goto restartApp;
+        }
+    }
+}
